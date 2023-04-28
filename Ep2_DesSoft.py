5696347b598385d2c691d454dd6beae9d7b6dd1e
zorrzi <eduardovz@al.insper.edu.br>
2023-04-28 11:08:34 -0300

    função: define_posicoes

--- Ep2_DesSoft.py
+++ Ep2_DesSoft.py
@@ -1,1 +1,50 @@
-print('comecando o trabalho')
+def define_posicoes(linha, coluna, orientacao, tamanho):
+
+    lista = []
+
+    if tamanho == 1:
+        lista.append([linha, coluna])
+    elif tamanho == 2:
+        if orientacao == 'vertical':
+            lista.append([linha,coluna])
+            lista.append([linha + 1, coluna])
+        else:
+            if linha == 0 and coluna == 0:
+                lista.append([linha,coluna])
+                lista.append([linha,coluna + 1])
+            else:
+                lista.append([linha,coluna + 1])
+                lista.append([linha,coluna + 2])
+    elif tamanho == 3:
+        if orientacao == 'vertical':
+            lista.append([linha, coluna])
+            lista.append([linha + 1, coluna])
+            lista.append([linha + 2, coluna])
+        else:
+            if linha == 0 and coluna == 0:
+                lista.append([linha, coluna])
+                lista.append([linha, coluna + 1])
+                lista.append([linha, coluna + 2])
+            else:
+                lista.append([linha, coluna +1])
+                lista.append([linha, coluna +2])
+                lista.append([linha, coluna +3])
+    elif tamanho == 4:
+        if orientacao == 'vertical':
+            lista.append([linha, coluna])
+            lista.append([linha + 1, coluna])
+            lista.append([linha + 2, coluna])
+            lista.append([linha + 3, coluna])
+        else:
+            if linha == 0 and coluna == 0:
+                lista.append([linha, coluna])
+                lista.append([linha, coluna + 1])
+                lista.append([linha, coluna + 2])
+                lista.append([linha, coluna + 3])
+            else:
+                lista.append([linha, coluna +1])
+                lista.append([linha, coluna +2])
+                lista.append([linha, coluna +3])
+                lista.append([linha, coluna +4])
+
+    return lista
